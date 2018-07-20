e945482e4bb4753776f70b6127fa2f8c930ebf6a
Rodrigo Tessari <rodrigo.tessari@outlook.com>
2018-07-20 17:57:43 -0300

    initial commit

---
+++ sql/System.Drawing.sql
@@ -0,0 +1,14 @@
+USE [ProlexNet]
+GO
+
+ALTER DATABASE [ProlexNet] SET TRUSTWORTHY ON;
+GO
+
+CREATE ASSEMBLY [System.Drawing] 
+AUTHORIZATION [dbo]
+FROM 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Drawing.dll'
+WITH PERMISSION_SET = UNSAFE;
+GO
+
+ALTER DATABASE [ProlexNet] SET TRUSTWORTHY OFF;
+GO
